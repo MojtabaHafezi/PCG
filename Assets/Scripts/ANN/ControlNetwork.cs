19fb11989d0125fd76b0cb95a5078fddba748bd4
Mojtaba Hafezi <mojtaba.hafezi@hotmail.de>
2017-04-07 19:04:29 +0200

    started programming ai

---
+++ Assets/Scripts/ANN/ControlNetwork.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlNetwork
+{
+	public NeuralNetwork Input, Output, Hidden;
+
+
+	public void Initialize (int inputNodes, int outputNodes, int hiddenNodes)
+	{
+		//Initialize Input Layer
+		Input.numberNodes = inputNodes;
+		Input.numberChildNodes = hiddenNodes;
+		Input.numberParentNodes = 0;
+		Input.Initialization (inputNodes, null, Hidden);
+		Input.RandomizeWeights ();
+
+		//Initialize Hidden Layer
+		Hidden.numberNodes = hiddenNodes;
+		Hidden.numberChildNodes = outputNodes;
+		Hidden.numberParentNodes = inputNodes;
+		Hidden.Initialization (hiddenNodes, Input, Output);
+		Hidden.RandomizeWeights ();
+
+		//Initialize Output Layer
+		Output.numberNodes = outputNodes;
+		Output.numberChildNodes = 0;
+		Output.numberParentNodes = hiddenNodes;
+		Output.Initialization (outputNodes, Hidden, null);
+	}
+
+	public void GiveInput (int i, double value)
+	{
+		if ((i >= 0 && (i < Input.numberNodes))) {
+			Input.calcOutput [i] = value;
+		}
+	}
+
+	//returns the specified output neuron or int max value when the neuron doesnt exist
+	public double GetOutput (int i)
+	{
+		if (i >= 0 && (i < Output.numberNodes)) {
+			return Output.calcOutput [i];
+		}
+		return (double)int.MaxValue;
+	}
+
+	//index and value for the neuron is taken and set at the desired position
+	public void DesiredOutput (int i, double value)
+	{
+		if ((i >= 0) && (i < Output.numberNodes)) {
+			Output.desiredOutput [i] = value;
+		}
+	}
+	//calculates the outputs for using the method GetOutput
+	public void FeedForward ()
+	{
+		Input.CalculatedOutput ();
+		Hidden.CalculatedOutput ();
+		Output.CalculatedOutput ();
+	}
+
+	//after the output has been calculated, the weights need to be adjusted
+	public void BackPropogate ()
+	{
+		Output.CalculateErrors ();
+		Hidden.CalculateErrors ();
+		Hidden.AdjustWeights ();
+		Output.AdjustWeights ();
+	}
+
+	//determine which output neuron gets fired (maximum value of all output neurons)
+	public int GetMaxOutput ()
+	{
+		int id;
+		double max;
+		max = Output.calcOutput [0];
+		id = 0;
+		for (int i = 1; i < Output.numberNodes; i++) {
+			if (Output.calcOutput [i] > max) {
+				max = Output.calcOutput [i];
+				id = i;
+			}
+		}
+		return id;
+	}
+
+	//returns error value: calced output - desired using the mean square error function
+	public double CalculateError ()
+	{
+		double error = 0;
+		for (int i = 0; i < Output.numberNodes; i++) {
+			float value = (float)(Output.calcOutput [i] - Output.desiredOutput [i]);
+			error += Mathf.Pow (value, 2f);
+		}
+		error = error / Output.numberNodes;
+		return error;
+	}
+
+	//set the learning rate for all layers
+	public void SetLearningRate (double rate)
+	{
+		Input.learningRate = Hidden.learningRate = Output.learningRate = rate;
+	}
+	//only output will use the linear activation function
+	public void SetLinearRule (bool value)
+	{
+		Input.linear = Output.linear = Hidden.linear = value;
+	}
+	//activate the momentum and give the parameter
+	public void SetMomentum (bool useMomentum, double factor)
+	{
+		Input.momentum = Output.momentum = Hidden.momentum = useMomentum;
+		Input.momentumValue = Hidden.momentumValue = Output.momentumValue = factor;
+	}
+
+	public string ToStringData ()
+	{
+		string message;
+		message += "Input Layer: \n";
+		message += "Node Values: \n";
+		for (int i = 0; i < Input.numberNodes; i++) {
+			message += Input.calcOutput [i] + ";";
+		}
+		message += "Weights: \n";
+		for (int i = 0; i < Input.numberNodes; i++) {
+			for (int j = 0; j < Input.numberChildNodes; j++) {
+				message += Input.weights [i] [j] + ";";
+			}
+		}
+		message += "Bias Weights: \n";
+		for (int j = 0; j < Input.numberChildNodes; j++) {
+			message += Input.biasWeights [j] + ";";
+		}
+
+
+
+		
+	}
+
+}
---
+++ Assets/Scripts/ANN/NeuralNetwork.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NeuralNetwork
+{
+	//number of nodes for that specific layer
+	public int numberNodes;
+	public int numberChildNodes;
+	public int numberParentNodes;
+	//false: logistic activation, true: linear
+	public bool linear;
+	//use momentum to find global minima
+	public bool momentum;
+	public double momentumValue;
+	//learning rate for weight adjustment
+	public double learningRate;
+
+	//stores the calculated output
+	public double[] calcOutput;
+	//in training we have desired outputs to calculate errors
+	public double[] desiredOutput;
+	//saves the errors for each neuron
+	public double[] errors;
+	//array of bias weights to each neuron
+	public double[] biasWeights;
+	//bias is a dummy input with either 1 or -1
+	public double[] biasValues;
+
+	//two-dimensional array of weight values connecting parent - child nodes
+	public double[][] weights;
+	//saves the adjustments done to the weights
+	public double[][] weightChanges;
+
+	public NeuralNetwork parent, child;
+
+	public NeuralNetwork ()
+	{
+		momentumValue = 0.8;
+		momentum = false;
+		linear = false;
+		parent = null;
+		child = null;
+	}
+
+	//initialize all arrays with 0s and the bias with 1 or -1
+	public void Initialization (int number, NeuralNetwork par, NeuralNetwork chi)
+	{
+		if (par != null)
+			this.parent = par;
+		if (chi != null)
+			this.child = chi;
+		for (int i = 0; i < numberNodes; i++) {
+			calcOutput [i] = desiredOutput [i] = errors [i] = 0;
+			if (child != null) {
+				//since every parent node is connected to every child node
+				for (int j = 0; j < numberChildNodes; j++) {
+					weights [i] [j] = weightChanges [i] [j] = 0;
+				}
+			}
+		}
+		if (child != null) {
+			for (int k = 0; k < numberChildNodes; k++) {
+				biasValues [k] = 1;
+				biasWeights [k] = 0;
+			}
+		}
+	}
+
+	//Randomize the weights between -1 and 1 - also in bias
+	public void RandomizeWeights ()
+	{
+		int min = 0;
+		int max = 201;
+		int number;
+
+		for (int i = 0; i < numberNodes; i++) {
+			for (int j = 0; j < numberChildNodes; j++) {
+				number = Random.Range (min, max);
+				weights [i] [j] = number / 100.0 - 1;
+			}
+		}
+		for (int k = 0; k < numberChildNodes; k++) {
+			number = Random.Range (min, max);
+			biasWeights [k] = number / 100.0 - 1;
+		}
+
+	}
+
+	//Calcute the net input of all parent nodes  + bias - use sigmoid function for output
+	//if linear is activated then the output layer will return the linear activation function
+	public void CalculatedOutput ()
+	{
+		double sum;
+		if (parent != null) {
+			for (int i = 0; i < numberNodes; i++) {
+				sum = 0;
+				for (int j = 0; j < numberParentNodes; j++) {
+					sum += parent.calcOutput [j] * parent.weights [j] [i];
+				}
+				sum += parent.biasValues [i] * parent.biasWeights [i];
+
+				if (child == null && linear)
+					calcOutput [i] = sum;
+				else
+					calcOutput [i] = (1.0f / (1 + Mathf.Exp ((float)-sum)));
+			}
+		}
+	}
+	//calculate the errors
+	public void CalculateErrors ()
+	{
+		double sum;
+		//output layer has no children
+		if (child == null) {
+			for (int i = 0; i < numberNodes; i++) {
+				//derivate of sigmoid function = sigm * (1-sigm)
+				errors [i] = (desiredOutput [i] - calcOutput [i])
+				* calcOutput [i] * (1.0 - calcOutput [i]);
+			}
+			//input has no parent
+		} else if (parent == null) {
+			for (int i = 0; i < numberNodes; i++) {
+				errors [i] = 0.0;
+			}
+			//hidden layer
+		} else {
+			for (int i = 0; i < numberNodes; i++) {
+				sum = 0;
+				for (int j = 0; j < numberChildNodes; j++) {
+					sum += child.errors [i] * weights [i] [j];
+				}
+				errors [i] = sum * calcOutput [i] * (1.0 - calcOutput [i]);
+			}
+		}
+	}
+
+	//Adjust the weights
+	public void AdjustWeights ()
+	{
+		double value;
+		//only hidden or input layer are changed
+		if (child != null) {
+			for (int i = 0; i < numberNodes; i++) {
+				for (int j = 0; j < numberChildNodes; j++) {
+					value = learningRate * child.errors [j] * calcOutput [j];
+					//if momentum - previous epochs weight changes are also added
+					if (momentum) {
+						weights [i] [j] += value + momentumValue * weightChanges [i] [j];
+						weightChanges [i] [j] = value;
+						//if no momentum is used -> no need to save epoch
+					} else {
+						weights [i] [j] += value;
+					}
+				}
+			}
+			for (int k = 0; k < numberChildNodes; k++) {
+				biasWeights [k] += learningRate * child.errors [k] * biasValues [k];
+			}
+		}
+	}
+		
+}
